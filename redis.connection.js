400881a0d970e1e260d7c4b8a63104451d357eb2
dipankrr <dipankar.laptop@gmail.com>
2026-05-04 12:09:46 +0530

    redis edit

--- redis.connection.js
+++ redis.connection.js
@@ -3,8 +3,7 @@ import Redis from "ioredis";
 export function createRedisConnection() {
   return process.env.REDIS_URL
     ? new Redis(process.env.REDIS_URL)
-    : new Redis("redis://default:ZbNOIsamIdLjIcUP2wZdNx85S2YILHH3@redis-15809.c264.ap-south-1-1.ec2.cloud.redislabs.com:15809")
-    // : new Redis({ host: "localhost", port: 6379 });
+    : new Redis({ host: "localhost", port: 6379 });
 }
 
 // export const redis = new createRedisConnection();
